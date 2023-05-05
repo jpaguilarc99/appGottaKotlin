dbeb264599b36d5cc6737af5d8e39bf7efd110a1
jpaguilarc99 <83897678+jpaguilarc99@users.noreply.github.com>
2023-05-05 00:17:04 -0500

    added data picker logic

--- src/main/java/com/jpaguilarc/gottaformapplication/OneContentActivity.kt
+++ src/main/java/com/jpaguilarc/gottaformapplication/OneContentActivity.kt
@@ -3,10 +3,13 @@ package com.jpaguilarc.gottaformapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class OneContentActivity : AppCompatActivity() {
 
     private lateinit var btnBack: Button
+    private lateinit var etDate: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_content)
@@ -16,11 +19,21 @@ class OneContentActivity : AppCompatActivity() {
 
     private fun initComponents() {
         btnBack = findViewById(R.id.btnBack)
+        etDate = findViewById(R.id.etDate)
     }
 
     private fun initListeners() {
-        btnBack.setOnClickListener {
-            onBackPressed()
-        }
+        btnBack.setOnClickListener { onBackPressed() }
+        etDate.setOnClickListener { showDatePickerDialog() }
+    }
+
+    //Selector de Fecha
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    fun onDateSelected(day:Int, month:Int, year:Int){
+        etDate.setText("$day / $month / $year")
     }
 }
