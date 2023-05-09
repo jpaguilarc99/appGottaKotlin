131d97a13877ff215be6e76cd8460777c5878811
jpaguilarc99 <83897678+jpaguilarc99@users.noreply.github.com>
2023-05-08 22:28:55 -0500

    added fourContent and first part of fiveContent

---
+++ src/main/java/com/jpaguilarc/gottaformapplication/FiveContentActivity.kt
@@ -0,0 +1,48 @@
+package com.jpaguilarc.gottaformapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class FiveContentActivity : AppCompatActivity() {
+
+    private lateinit var btnBackFive: Button
+    private lateinit var btnNextFive: Button
+    private lateinit var etDate: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_five_content)
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        btnBackFive = findViewById(R.id.btnBackFive)
+        btnNextFive = findViewById(R.id.btnNextFive)
+        etDate = findViewById(R.id.etDate)
+    }
+
+    private fun initListeners() {
+        btnBackFive.setOnClickListener { onBackPressed() }
+        btnNextFive.setOnClickListener { navigateToSix() }
+        etDate.setOnClickListener { showDatePickerDialog() }
+    }
+
+    private fun navigateToSix() {
+        val intent = Intent(this, FiveContentActivity::class.java)
+        startActivity(intent)
+    }
+
+    //Selector de Fecha
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    private fun onDateSelected(day:Int, month:Int, year:Int){
+        etDate.setText("$day / $month / $year")
+    }
+}
--- src/main/java/com/jpaguilarc/gottaformapplication/FourContentActivity.kt
+++ src/main/java/com/jpaguilarc/gottaformapplication/FourContentActivity.kt
@@ -1,11 +1,34 @@
 package com.jpaguilarc.gottaformapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FourContentActivity : AppCompatActivity() {
+
+    private lateinit var btnBackFour: Button
+    private lateinit var btnNextFour: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_four_content)
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        btnBackFour = findViewById(R.id.btnBackFour)
+        btnNextFour = findViewById(R.id.btnNextFour)
+    }
+
+    private fun initListeners() {
+        btnBackFour.setOnClickListener { onBackPressed() }
+        btnNextFour.setOnClickListener { navigateToFive() }
+    }
+
+    private fun navigateToFive() {
+        val intent = Intent(this, FiveContentActivity::class.java)
+        startActivity(intent)
     }
 }
